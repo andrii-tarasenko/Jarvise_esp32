b9b6c9ddb1e7f9a4ef4bbd472ae012e9da89cedc
andrii-tarasenko <tarasenk.andrey@gmail.com>
2026-04-05 18:55:23 +0300

    update NetWorkManager

--- src/NetworkManager.cpp
+++ src/NetworkManager.cpp
@@ -8,21 +8,28 @@ void NetworkManager::connect() {
     if (WiFi.status() == WL_CONNECTED)
         return;
 
-    WiFi.mode(WIFI_STA);
+    Serial.print("Connecting to WiFi: ");
+    Serial.println(WIFI_SSID);
+
+    WiFi.mode(WIFI_STA); // Режим клієнта
     WiFi.begin(WIFI_SSID, WIFI_PASS);
 
     unsigned long start = millis();
 
     while (WiFi.status() != WL_CONNECTED && millis() - start < 15000) {
         delay(500);
+        Serial.print(".");
     }
 
-      if (WiFi.status() == WL_CONNECTED) {
-         Serial.println("WiFi connected ✅");
+    if (WiFi.status() == WL_CONNECTED) {
+        Serial.println("\nWiFi connected ✅");
+        Serial.print("IP address: ");
         Serial.println(WiFi.localIP());
-      } else {
-        Serial.println("WiFi connect FAILED ❌");
-      }
+    } else {
+        Serial.println("\nWiFi connect FAILED ❌");
+    }
 }
 
-bool NetworkManager::isConnected() { return WiFi.status() == WL_CONNECTED; }
+bool NetworkManager::isConnected() {
+    return WiFi.status() == WL_CONNECTED;
+}
