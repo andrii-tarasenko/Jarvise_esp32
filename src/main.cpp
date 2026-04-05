c52b70205030aed3696d590999bd94bafef0fd5a
andrii-tarasenko <tarasenk.andrey@gmail.com>
2026-04-05 18:46:34 +0300

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -15,34 +15,38 @@ const int SEND_INTERVAL = 5000;
 
 void setup() {
   Serial.begin(115200);
+
   delay(3000);
-  Serial.println("\n🚀 Jarvise ESP32 Advanced Diagnostic Mode Starting...");
 
   networkManager = new NetworkManager();
   networkManager->connect();
 
   inverterReader = new InverterReader(INVERTER_RX_PIN, INVERTER_TX_PIN);
-  // Сканер почнеться автоматично в loop() через process()
-  
-  apiClient = new ApiClient(SERVER_HOST);
+  inverterReader->begin();
 
-  logRemote("🚀 Advanced Scanner Started: Baud (2400/9600) x Polarity (Normal/Inverted)");
+  apiClient = new ApiClient(SERVER_HOST);
 }
 
 void loop() {
   if (!networkManager->isConnected()) {
     networkManager->connect();
-  }
+  } //TODO else add cash for data
 
   inverterReader->process();
 
   if (millis() - lastSendTime > SEND_INTERVAL) {
     if (networkManager->isConnected()) {
       InverterData data = inverterReader->getData();
+
       if (data.isValid) {
-        apiClient->sendInverterData(data);
+        if (apiClient->sendInverterData(data)) {
+          logRemote("☁️ Data were sent");
+        } else {
+          logRemote("⚠️ Data were not sent");
+        }
       }
     }
+
     lastSendTime = millis();
   }
 }
